0a064420c3e69f0b79d61a46ec1519f4586f0528
Robert Zanceanu <robert@sprybase.com>
2021-06-05 19:49:09 +0300

    Save csv files on AWS S3

--- controllers/export-data-to-csv.js
+++ controllers/export-data-to-csv.js
@@ -2,6 +2,8 @@ import { getSongsByConditions, getListenedSongsByConditions, getLikedSongsByCond
 import { SongsModel, ListenedModel, LikesModel } from '../models/index.js'
 import fs from 'fs'
 import json2csv from 'json2csv'
+import { s3 } from '../index.js'
+
 const { Parser } = json2csv
 
 const transformJsonToCsv = async (json, fields) => {
@@ -19,6 +21,53 @@ const writeCsvToFile = async (csv, path) => {
     })
 }
 
+const saveFilesToAWSS3 = async () => {
+    const songsCsv = fs.readFileSync('./songs.csv')
+    const params = {
+        Bucket: process.env.AWS_S3_BUCKET_NAME,
+        Key: 'songs.csv',
+        Body: songsCsv
+    }
+    console.log('songs', songsCsv)
+    s3.upload(params, (err, data) => {
+        if (err) {
+            console.log('err', err)
+            return
+        }
+        console.log(data)
+    })
+
+    const listenedCsv = fs.readFileSync('./listened.csv')
+    const paramsListened = {
+        Bucket: process.env.AWS_S3_BUCKET_NAME,
+        Key: 'listened.csv',
+        Body: listenedCsv
+    }
+    console.log('listened', listenedCsv)
+    s3.upload(paramsListened, (err, data) => {
+        if (err) {
+            console.log('err', err)
+            return
+        }
+        console.log(data)
+    })
+
+    const likedCsv = fs.readFileSync('./liked.csv')
+    const paramsLiked = {
+        Bucket: process.env.AWS_S3_BUCKET_NAME,
+        Key: 'liked.csv',
+        Body: likedCsv
+    }
+    console.log('likedCsv', likedCsv)
+    s3.upload(paramsLiked, (err, data) => {
+        if (err) {
+            console.log('err', err)
+            return
+        }
+        console.log(data)
+    })
+}
+
 export const exportToCsv = async (req, res) => {
     //create songs csv
     const songsResult = await getSongsByConditions({}, 0, 1)
@@ -26,7 +75,7 @@ export const exportToCsv = async (req, res) => {
         res.status(songsResult.status).json({ message: 'Error' })
     }
     const songs = songsResult.result.documents
-    const songsCsvFields = ['_id', 'name', 'genre._id', 'genre.name', 'artist._id', 'artist.name']
+    const songsCsvFields = ['_id', 'name', 'genre._id', 'genre.name', 'artist._id', 'artist.name', 'image']
     const songsCsv = await transformJsonToCsv(songs, songsCsvFields)
     await writeCsvToFile(songsCsv, './songs.csv')
     console.log(songsCsv)
@@ -36,6 +85,7 @@ export const exportToCsv = async (req, res) => {
     //create liked csv
     const likedResult = await getLikedSongsByConditions({})
     if (likedResult.status >= 400) {
+        console.log('liked', likedResult)
         res.status(likedResult.status).json({ message: likedResult.result.message })
         return
     }
@@ -44,9 +94,10 @@ export const exportToCsv = async (req, res) => {
     const likedCsv = await transformJsonToCsv(likedSongs, likedAndListenedCsvFields)
     await writeCsvToFile(likedCsv, './liked.csv')
 
-    // //create listened csv
+    //create listened csv
     const listenedResult = await getListenedSongsByConditions({})
     if (listenedResult.status >= 400) {
+        console.log('listened', listenedResult)
         res.status(listenedResult.status).json({ message: listenedResult.result.message })
         return
     }
@@ -54,6 +105,8 @@ export const exportToCsv = async (req, res) => {
     const listenedSongs = listenedResult.result
     const listenedCsv = await transformJsonToCsv(listenedSongs, likedAndListenedCsvFields)
     await writeCsvToFile(listenedCsv, './listened.csv')
+
+    await saveFilesToAWSS3()
 
     res.json({
         songs,
--- index.js
+++ index.js
@@ -5,6 +5,7 @@ import cors from 'cors';
 import mongoose from 'mongoose';
 import { ytUrlCron } from './yturl-cron.js';
 import cron from 'node-cron'
+import AWS from 'aws-sdk'
 
 dotenv.config()
 
@@ -12,6 +13,24 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 const port = process.env.PORT;
+
+export const s3 = new AWS.S3({
+    accessKeyId: process.env.AWS_ACCESS_KEY,
+    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+})
+
+const params = {
+    Bucket: process.env.AWS_S3_BUCKET_NAME,
+    // CreateBucketConfiguration: {
+    //     // Set your region here
+    //     LocationConstraint: "us-east-1"
+    // }
+}
+
+s3.createBucket(params, function(err, data) {
+    if (err) console.log(err, err.stack);
+    else console.log('Bucket Created Successfully', data);
+})
 
 const mongodbUri = `mongodb+srv://${process.env.MONGODB_USERNAME}:${process.env.MONGODB_PASSWORD}@cluster0.h3y2c.mongodb.net/main_DB?retryWrites=true&w=majority`;
 mongoose.connect(mongodbUri, { useNewUrlParser: true, useUnifiedTopology: true });
